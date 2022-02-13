debdf20f1dc185c90bb81112430a9c9ce0dc74d6
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-02-13 07:25:58 +0100

    Módulo D - Aula 11 - Condições (parte 1)

---
+++ Modulo D/Aula 11/ex001.js
@@ -0,0 +1,9 @@
+var vel = 78.2;
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+
+// Condição simples
+if (vel > 60.0) {
+  console.log('Você ultrapassou a velocidade permitida. MULTADO!!');
+};
+
+console.log('Dirija sempre usando cinto de segurança!');
---
+++ Modulo D/Aula 11/ex002.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil';
+console.log(`Nascido em ${pais}`);
+
+// Condição composta
+if (pais != 'Brasil') {
+  console.log('Estrangeiro');
+} else {
+  console.log('Brasileiro');
+};
