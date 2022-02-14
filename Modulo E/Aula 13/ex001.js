4fc90a912cae5259f8df0df1002b04a42d6b0f92
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-02-14 17:40:45 +0100

    Módulo E - Aula 13 - Repetições (Parte 1)

---
+++ Modulo E/Aula 13/ex001.js
@@ -0,0 +1,14 @@
+var c1 = 1;
+
+// Teste lógico com início
+while (c1 <= 3) {
+  console.log(`Passo ${c1} - while`);
+  c1++;
+};
+
+var c2 = 1;
+// Teste lógico no final
+do {
+  console.log(`Passo ${c2} - do-while`);
+  c2++;
+} while(c2 <= 6);
