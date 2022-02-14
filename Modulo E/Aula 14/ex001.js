34fdbe1de984e998d90a48935798a3708e7620fc
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-02-14 21:26:19 +0100

    Módulo E - Aula 14 - Repetições (Parte 2)

---
+++ Modulo E/Aula 14/ex001.js
@@ -0,0 +1,11 @@
+console.log('Vai começar a contar até 10.');
+for (i = 1; i <= 10; i++) {
+  console.log(`Passo ${i}`);
+};
+console.log('Terminou de contar até 10.');
+
+console.log('Vai começar a contar até 1.');
+for (i = 10; i>= 1; i--) {
+  console.log(`Passo ${i}`);
+};
+console.log('Terminou de contar até 1.');
